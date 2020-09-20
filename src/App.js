3396d87bb38153ca0041c1a50df7f8ed71868fd2
HamoudaBenAbdennebi <hamoudabenabdennebi77@gmail.com>
2020-09-20 15:05:31 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Contact from "./contact";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+function App() {
+  return (
+    <div className="main">
+      <Contact />
+    </div>
+  );
+}
+
+export default App;
